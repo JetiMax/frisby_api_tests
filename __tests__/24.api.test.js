ce3dd44e5eaac6bd5fab3cc8be0c0745f8225839
Max <91092934+JetiMax@users.noreply.github.com>
2023-07-10 12:40:36 +0300

    Update 24.api.test.js

--- __tests__/24.api.test.js
+++ __tests__/24.api.test.js
@@ -1,22 +1,35 @@
-const frisby = require('frisby');
+const frisby = require("frisby");
 const Joi = frisby.Joi;
-const moment = require('moment');
 
-describe("Day24", () => {
-it('Determine day two days from now', () => {
-    return frisby
-        .get('http://worldtimeapi.org/api/ip')
-        .expect('status', 200)
-        .expect('jsonTypes', {
-            abbreviation: Joi.string(),
-            client_ip: Joi.string()
-            
-        })
-        .then((response) => {
-            const currentDate = moment(response.json.datetime);
-            const futureDate = currentDate.add(2, 'days').format('dddd');
 
-            console.log('Two days from now is: ' + futureDate);
-        });
+describe("day24", () => {
+    it("random dad joke", function () {
+             return frisby
+
+            .get(`https://icanhazdadjoke.com/search?term={$randomWord}`)
+            .expect("status", 403)
+    })
 });
-});
+
+    it("moments in time", function () {
+        return frisby
+            .get('http://worldtimeapi.org/api/ip')
+            .expect("status", 200)
+            .expect('jsonTypes', [{
+                  "abbreviation": "MSK",
+                  "client_ip": "188.242.254.148",
+                  "datetime": Joi.string().required(),
+                  "day_of_week": 1,
+                  "day_of_year": 184,
+                  "dst": false,
+                  "dst_from": null,
+                  "dst_offset": 0,
+                  "dst_until": null,
+                  "raw_offset": 10800,
+                  "timezone": "Europe/Moscow",
+                  "utc_datetime": Joi.string().required(),
+                  "utc_offset": "+03:00",
+                  "week_number": 27
+                }] )
+            .then((result) => console.log(result.json))
+    })
