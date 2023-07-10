ce1f44ddf7752819c96f511e54dec40692dd948a
Max <91092934+JetiMax@users.noreply.github.com>
2023-07-10 13:18:33 +0300

    Update 24.api.test.js

--- __tests__/24.api.test.js
+++ __tests__/24.api.test.js
@@ -1,35 +1,34 @@
 const frisby = require("frisby");
 const Joi = frisby.Joi;
+const randomWord = 'term'
 
-
-describe("day24", () => {
+describe("Day24", () => {
     it("random dad joke", function () {
-             return frisby
+        return frisby
 
-            .get(`https://icanhazdadjoke.com/search?term={$randomWord}`)
+            .get(`https://icanhazdadjoke.com/search?term=${randomWord}`)
             .expect("status", 403)
     })
 });
 
-    it("moments in time", function () {
-        return frisby
-            .get('http://worldtimeapi.org/api/ip')
-            .expect("status", 200)
-            .expect('jsonTypes', [{
-                  "abbreviation": "MSK",
-                  "client_ip": "188.242.254.148",
-                  "datetime": Joi.string().required(),
-                  "day_of_week": 1,
-                  "day_of_year": 184,
-                  "dst": false,
-                  "dst_from": null,
-                  "dst_offset": 0,
-                  "dst_until": null,
-                  "raw_offset": 10800,
-                  "timezone": "Europe/Moscow",
-                  "utc_datetime": Joi.string().required(),
-                  "utc_offset": "+03:00",
-                  "week_number": 27
-                }] )
-            .then((result) => console.log(result.json))
-    })
+it("moments in time", function () {
+    return frisby
+        .get('http://worldtimeapi.org/api/ip')
+        .expect("status", 200)
+        .expect('jsonTypes', [{
+            "abbreviation": "MSK",
+            "client_ip": "188.242.254.148",
+            "datetime": Joi.string().required(),
+            "day_of_week": 1,
+            "dst": false,
+            "dst_from": null,
+            "dst_offset": 0,
+            "dst_until": null,
+            "raw_offset": 10800,
+            "timezone": "Europe/Moscow",
+            "utc_datetime": Joi.string().required(),
+            "utc_offset": "+03:00",
+        }] )
+        .then((result) => console.log(result.json))
+
+});
